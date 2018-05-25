51022de9336b57769c7a5d15e716e5d2f2320d87
jeffrey levesque <jeff1evesque@yahoo.com>
2018-05-25 17:25:01 -0400

    #14: basic.R, use multiple verbose 'colsplit'

--- analysis/basic.R
+++ analysis/basic.R
@@ -36,12 +36,32 @@ download_source(
 df1 <- load_df('./dataset/train_1.csv')
 df2 <- load_df('./dataset/train_2.csv')
 
-## explode column: first column (i.e. 'Page') will become four columns
+## explode column: first column into two general columns
 df1 <- cbind(
-    colsplit(df1$Page, '_', reshaped_page),
-    df1[,-which(names(df1) == "Page")]
+    colsplit(df1$Page, '.wikipedia.org_', c('first', 'second')),
+    df1[,-which(names(df1) == 'Page')]
 )
 df2 <- cbind(
-  colsplit(df2$Page, '_', reshaped_page),
-  df2[,-which(names(df2) == "Page")]
+  colsplit(df2$Page, '.wikipedia.org_', c('first', 'second')),
+  df2[,-which(names(df2) == 'Page')]
+)
+
+## explode column: second column into Article, and Language columns
+df1 <- cbind(
+  colsplit(df1$Page, pattern=perl('_(?=[^_]+$)'), c('Article', 'Language')),
+  df1[,-which(names(df1) == 'first')]
+)
+df2 <- cbind(
+  colsplit(df2$Page, pattern=perl('_(?=[^_]+$)'), c('Article', 'Language')),
+  df2[,-which(names(df2) == 'first')]
+)
+
+## explode column: info column into Access, and Agent columns
+df1 <- cbind(
+  colsplit(df1$second, '_', c('Access', 'Agent')),
+  df1[,-which(names(df1) == 'second')]
+)
+df2 <- cbind(
+  colsplit(df2$second, '_', c('Access', 'Agent')),
+  df2[,-which(names(df2) == 'second')]
 )
