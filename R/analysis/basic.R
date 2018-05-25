1ce175c28237d9d04a6474eb42bfcff37056acba
jeffrey levesque <jeff1evesque@yahoo.com>
2018-05-24 22:03:46 -0400

    #10: basic.R, initially load dataset into df's

---
+++ R/analysis/basic.R
@@ -0,0 +1,18 @@
+##
+## basic.R, analyze the the following wikipedia dataset:
+##
+##     - https://www.dropbox.com/s/x14f3bg8flej1n7/train_1.csv?dl=1
+##     - https://www.dropbox.com/s/o2df10dnyt3bg02/train_2.csv?dl=1
+##
+
+## utility functions
+source('../utility/download.R')
+source('../utility/dataframe.R')
+
+## download datasets
+download('https://www.dropbox.com/s/x14f3bg8flej1n7/train_1.csv?dl=1', './train_1.csv')
+download('https://www.dropbox.com/s/o2df10dnyt3bg02/train_2.csv?dl=1', './train_2.csv')
+
+## ingest dataset into dataframe
+df1 <- load_df('./train_1.csv')
+df2 <- load_df('./train_2.csv')
