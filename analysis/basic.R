a80d1c97530511e8362d96cc36de6f4f71967189
jeffrey levesque <jeff1evesque@yahoo.com>
2018-05-25 07:55:00 -0400

    #10: create 'dataset/' directory if not exists

--- analysis/basic.R
+++ analysis/basic.R
@@ -10,11 +10,14 @@ cwd <- dirname(dirname(rstudioapi::getSourceEditorContext()$path))
 setwd(cwd)
 
 ## utility functions
-library('utility', lib.loc = paste(getwd(), sep='', '/packages'))
+library('utility', lib.loc = paste(cwd, sep='', '/packages'))
+
+## dataset directory
+dir.create(file.path(cwd, 'dataset'), showWarnings = FALSE)
 
 ## download datasets
-download('https://www.dropbox.com/s/x14f3bg8flej1n7/train_1.csv?dl=1', './train_1.csv')
-download('https://www.dropbox.com/s/o2df10dnyt3bg02/train_2.csv?dl=1', './train_2.csv')
+download_source('https://www.dropbox.com/s/x14f3bg8flej1n7/train_1.csv?dl=1', './dataset/train_1.csv')
+download_source('https://www.dropbox.com/s/o2df10dnyt3bg02/train_2.csv?dl=1', './dataset/train_2.csv')
 
 ## create dataframes
 df1 <- load_df('./train_1.csv')
