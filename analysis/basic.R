be8da0ddc78dfd5114243ca3295c74661f6ea775
jeffrey levesque <jeff1evesque@yahoo.com>
2018-05-25 08:59:31 -0400

    #10: basic.R, fix 'load_df' arguments

--- analysis/basic.R
+++ analysis/basic.R
@@ -17,9 +17,15 @@ library('ist687utility')
 dir.create(file.path(cwd, 'dataset'), showWarnings = FALSE)
 
 ## download datasets
-download_source('https://www.dropbox.com/s/x14f3bg8flej1n7/train_1.csv?dl=1', './dataset/train_1.csv')
-download_source('https://www.dropbox.com/s/o2df10dnyt3bg02/train_2.csv?dl=1', './dataset/train_2.csv')
+download_source(
+    'https://www.dropbox.com/s/x14f3bg8flej1n7/train_1.csv?dl=1',
+    './dataset/train_1.csv'
+)
+download_source(
+    'https://www.dropbox.com/s/o2df10dnyt3bg02/train_2.csv?dl=1',
+    './dataset/train_2.csv'
+)
 
 ## create dataframes
-df1 <- load_df('./train_1.csv')
-df2 <- load_df('./train_2.csv')
+df1 <- load_df('./dataset/train_1.csv')
+df2 <- load_df('./dataset/train_2.csv')
