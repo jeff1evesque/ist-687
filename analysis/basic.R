3a5dd538451035623f10a50227c537bdf7296fa2
jeffrey levesque <jeff1evesque@yahoo.com>
2018-05-25 07:10:23 -0400

    #10: basic.R, movie script, and 'setwd'

--- analysis/basic.R
+++ analysis/basic.R
@@ -5,9 +5,12 @@
 ##     - https://www.dropbox.com/s/o2df10dnyt3bg02/train_2.csv?dl=1
 ##
 
+## set project cwd
+cwd <- dirname(dirname(rstudioapi::getSourceEditorContext()$path))
+setwd(cwd)
+
 ## utility functions
-source('../utility/download.R')
-source('../utility/dataframe.R')
+library('utility', lib.loc = paste(getwd(), sep='', '/packages'))
 
 ## download datasets
 download('https://www.dropbox.com/s/x14f3bg8flej1n7/train_1.csv?dl=1', './train_1.csv')
