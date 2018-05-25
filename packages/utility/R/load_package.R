9686e51e2d010623ff7a0da95a13064ecfe3d666
jeffrey levesque <jeff1evesque@yahoo.com>
2018-05-25 07:55:31 -0400

    #10: rename filenames to match function names

--- packages/utility/R/load_package.R
+++ packages/utility/R/load_package.R
@@ -1,5 +1,5 @@
 ##
-## package.R, install + install defined package
+## load_package.R, install + install defined package
 ##
 load_package <- function(package) {
   if (!require(package)) install.packages(package)
