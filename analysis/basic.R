3233aecaf4cb3b92d1b47defba57bc23345cd5f7
jeffrey levesque <jeff1evesque@yahoo.com>
2018-05-25 13:58:57 -0400

    #10: basic.R, split 'Page' into multiple cols

--- analysis/basic.R
+++ analysis/basic.R
@@ -13,6 +13,9 @@ setwd(cwd)
 devtools::install_local(paste(cwd, sep='', '/packages/ist687utility'))
 library('ist687utility')
 
+## load packages
+load_package('reshape2')
+
 ## dataset directory
 dir.create(file.path(cwd, 'dataset'), showWarnings = FALSE)
 
@@ -29,3 +32,14 @@ download_source(
 ## create dataframes
 df1 <- load_df('./dataset/train_1.csv')
 df2 <- load_df('./dataset/train_2.csv')
+
+## explode column: first column (i.e. 'Page') will become four columns
+df1 <- cbind(
+    colsplit(df1$Page, '_', c('name', 'project', 'access', 'agent')),
+    df1[,-which(names(df1) == "Page")]
+)
+
+df2 <- cbind(
+  colsplit(df1$Page, '_', c('name', 'project', 'access', 'agent')),
+  df2[,-which(names(df2) == "Page")]
+)
