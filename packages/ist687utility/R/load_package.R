ed1be28646a1e18d3dbf1c3263b4296c8775a2cb
jeffrey levesque <jeff1evesque@yahoo.com>
2018-05-25 22:02:57 -0400

    #17: load_packages.R, support array case

--- packages/ist687utility/R/load_package.R
+++ packages/ist687utility/R/load_package.R
@@ -2,6 +2,15 @@
 ## load_package.R, install + install defined package
 ##
 load_package <- function(package) {
-  if (!require(package, character.only=TRUE)) install.packages(package)
-  library(package, character.only = TRUE)
+  if (is.vector(package)) {
+    for (i in package){
+      if(!is.element(i, .package(all.available = TRUE)) ) {
+        install.packages(i)
+      }
+      library(i,character.only = TRUE)
+    }
+  } else {
+    if (!require(package, character.only=TRUE)) install.packages(package)
+    library(package, character.only = TRUE)
+  }
 }
