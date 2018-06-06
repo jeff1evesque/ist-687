40322400f581a9c2f4e5ced284bf007e6154a48b
jeffrey levesque <jeff1evesque@yahoo.com>
2018-06-06 19:08:11 -0400

    #44: top_indices.R, more robust parameters

--- packages/ist687utility/R/top_indices.R
+++ packages/ist687utility/R/top_indices.R
@@ -2,7 +2,7 @@
 ## top_indices.R, determine n largest values in a vector,
 ##     and return the corresponding vector index.
 ##
-top_indices <- function(x, end, start=0){
+top_indices <- function(x, end, start=0, increment=1){
   nums <- sort(x)
-  return(nums(c(seq(start, end, 1))))
+  return(nums(c(seq(start, end, increment))))
 }
