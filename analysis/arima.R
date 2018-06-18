664427f7aedda5e30d121d6bea4b1c993dfce8a3
jeffrey levesque <jeff1evesque@yahoo.com>
2018-06-18 17:20:18 -0400

    #74: arima.R, add initial file

---
+++ analysis/arima.R
@@ -0,0 +1,28 @@
+##
+## exploratory.R, exploratory analysis on wikipedia traffic:
+##
+##     - https://www.dropbox.com/s/x14f3bg8flej1n7/train_1.csv?dl=1
+##     - https://www.dropbox.com/s/o2df10dnyt3bg02/train_2.csv?dl=1
+##
+
+## set project cwd: only execute in RStudio
+if (nzchar(Sys.getenv('RSTUDIO_USER_IDENTITY'))) {
+  cwd <- dirname(dirname(rstudioapi::getSourceEditorContext()$path))
+  setwd(cwd)
+}
+
+## utility functions
+devtools::install_local(paste(cwd, sep='', '/packages/ist687utility'))
+library('ist687utility')
+
+## load packages
+if (!require('stringi')) install.packages('stringi', repos='http://cran.rstudio.com/')
+library(stringi)
+
+load_package(c('reshape2', 'zoo', 'Hmisc', 'ggplot2', 'grid', 'gridExtra'))
+
+## create dataframes
+df <- munge_ist687(
+  'https://www.dropbox.com/s/x14f3bg8flej1n7/train_2.csv?dl=1',
+  './dataset/train_2.csv'
+)
