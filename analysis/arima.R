3cf8ba8a7d97cce12a77a95a523a538999861b5c
jeffrey levesque <jeff1evesque@yahoo.com>
2018-06-24 20:44:04 -0400

    #82: arima.R, load train + test dataframes

--- analysis/arima.R
+++ analysis/arima.R
@@ -26,18 +26,22 @@ library(stringi)
 load_package(c('reshape2', 'zoo', 'Hmisc', 'forecast'))
 
 ## create dataframes
-df <- munge_ist687(
+df.train <- munge_ist687(
   'https://www.dropbox.com/s/x14f3bg8flej1n7/train-wikipedia.csv?dl=1',
   './dataset/train.csv'
 )
+df.test <- munge_ist687(
+  'https://www.dropbox.com/s/x14f3bg8flej1n7/test-wikipedia.csv?dl=1',
+  './dataset/test.csv'
+)
 
 ## row sums: determine sum for each row
-rSums <- rowSums(df[,-c(1:4)])
+rSums <- rowSums(df.train[,-c(1:4)])
 
 ## top 1-5, 10-15 articles
 for (i in c(1:5, 11:15)) {
   rIndex <- top_indices(rSums, i, i)
-  avgTop.m <- melt(df[rIndex, -c(2,4)])
+  avgTop.m <- melt(df.train[rIndex, -c(2,4)])
   article.name <- avgTop.m[i, which(colnames(avgTop.m) == 'Article')]
   article.access <- avgTop.m[i, which(colnames(avgTop.m) == 'Access')]
 
@@ -45,7 +49,7 @@ for (i in c(1:5, 11:15)) {
   article.name <- gsub('[^[^0-9A-Za-z_]', '', article.name, ignore.case = TRUE)
   article.access <- gsub('[^0-9A-Za-z_]', '', article.access, ignore.case = TRUE)
 
-  arima_ist687(
+  prediction <- arima_ist687(
     avgTop.m$value,
     ar=6,
     i=1,
